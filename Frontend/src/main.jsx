adc22b13bd440720615d85467c8482a59696f67a
kelvinvaghasiya <kelvin.vaghasiya@mrisoftware.com>
2024-10-15 11:33:53 +0530

    Created react routers and context api

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import UserProvider from "./Contexts/User.jsx"
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <BrowserRouter>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </BrowserRouter>
   </StrictMode>,
 )
