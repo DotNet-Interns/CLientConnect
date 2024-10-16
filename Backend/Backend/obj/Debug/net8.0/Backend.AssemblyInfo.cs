53482a1e35ea12006123e5f51f9931e43fc3b428
chiraggavande-mri <chirag.gavande@mrisoftware.com>
2024-10-16 09:34:19 +0530

    bkass

--- Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+113c3afcb03718fe1c7712885a5174fdc9afa562")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30b1f453faf9d59c5497a029cdbfc90f669b3e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
