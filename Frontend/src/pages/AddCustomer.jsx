a008c33e04b37297a2ad63da479c711e1ee80541
kelvinvaghasiya <kelvin.vaghasiya@mrisoftware.com>
2024-10-17 17:27:34 +0530

    Added validations in login and Add customer forms

--- Frontend/src/pages/AddCustomer.jsx
+++ Frontend/src/pages/AddCustomer.jsx
@@ -1,55 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as helpers from '../Utils/validation.js';
+// import CreateEmergenciesTable from '../../../utils/CreateEmergenciesTable';
 
 function AddCustomer() {
-      
+    console.log("AddCustomer")
+    const [customerData, setCustomerData] = useState({
+        firstName: "",
+        lastName: "",
+        Address: "",
+        Company_name: "",
+        Position: "",
+        email: "",
+        phone : ""
+    })
+
+
+
+    const [ValidationErrors, setValidationErrors] = useState({
+        firstName: "",
+        lastName: "",
+        Address: "",
+        Company_name: "",
+        Position: "",
+        email: "",
+        phone : ""
+    })
+
+    const handleChange = (event) => {
+        setCustomerData((prevValue) => {
+            return ({
+                ...prevValue,
+                [event.target.name]: event.target.value
+            })
+        })
+
+        setValidationErrors((prevValue) => {
+            return ({
+                ...prevValue,
+                [event.target.name]: ""
+            })
+        });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        helpers.validateCustomerData(customerData , setValidationErrors);
+    }
+
     return (
         <div className='container register-container'>
             <form>
                 <h2 style={{ textAlign: "center", marginBottom: "5vh" }}>Add New Customer</h2>
                 <div className='row '>
                     <div className="form-group col-sm-6 mt-2">
-                        <label for="First_Name">First Name</label>
-                        <input type="text" className="form-control" id="First_Name" placeholder="Enter First Name" />
-                        {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+                        <label htmlFor="First_Name">First Name</label>
+                        <input onChange={handleChange} type="text" name='firstName' value={customerData.firstName} className="form-control" id="First_Name" placeholder="Enter First Name" />
+                        <small>{ValidationErrors.firstName}</small>
                     </div>
                     <div className="form-group col-sm-6 mt-2">
-                        <label for="Last_Name">Last Name</label>
-                        <input type="text" className="form-control" id="Last_Name" placeholder="Enter Last Name" />
-                        {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+                        <label htmlFor="Last_Name">Last Name</label>
+                        <input onChange={handleChange} name='lastName' value={customerData.lastName} type="text" className="form-control" id="Last_Name" placeholder="Enter Last Name" />
+                        <small>{ValidationErrors.lastName}</small>
                     </div>
                 </div>
 
                 <div className="form-group">
-                    <label for="Cust_Address">Address</label>
-                    <input type="text" className="form-control" id="Cust_Address" placeholder="Enter Customer's Address" />
+                    <label htmlFor="Cust_Address">Address</label>
+                    <input onChange={handleChange} name='Address' value={customerData.Address} type="text" className="form-control" id="Cust_Address" placeholder="Enter Customer's Address" />
+                    <small>{ValidationErrors.Address}</small>
                 </div>
 
                 <div className="form-group">
-                    <label for="Cust_Company">Address</label>
-                    <input type="text" className="form-control" id="Cust_Company" placeholder="Enter Customer's Company Name" />
+                    <label htmlFor="Cust_Company">Company Name</label>
+                    <input onChange={handleChange} name='Company_name' value={customerData.Company_name} type="text" className="form-control" id="Cust_Company" placeholder="Enter Customer's Company Name" />
+                    <small>{ValidationErrors.Company_name}</small>
                 </div>
 
                 <div className="form-group">
-                    <label for="Cust_Position">Address</label>
-                    <input type="text" className="form-control" id="Cust_Position" placeholder="Enter Customer's Position in above Company" />
+                    <label htmlFor="Cust_Position">Position</label>
+                    <input onChange={handleChange} name='Position' value={customerData.Position} type="text" className="form-control" id="Cust_Position" placeholder="Enter Customer's Position in above Company" />
+                    <small>{ValidationErrors.Position}</small>
                 </div>
 
                 <div className="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
-                </div>
-                <div className="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    <label htmlFor="exampleInputEmail1">Email address</label>
+                    <input onChange={handleChange} name='email' value={customerData.email} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                    <small>{ValidationErrors.email}</small>
                 </div>
 
                 <div className="form-group">
-                    <label for="exampleInputPassword2">Confirm Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Confirm Password" />
+                    <label htmlFor="Phone_field">Phone</label>
+                    <input onChange={handleChange} name='phone' value={customerData.phone} className="form-control" id="Phone_field"  placeholder="Enter Phone Number" />
+                    <small>{ValidationErrors.phone}</small>
                 </div>
 
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button onClick={handleSubmit} className="btn btn-primary">Submit</button>
             </form>
         </div>
     )
