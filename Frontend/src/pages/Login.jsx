f8effa8174bf34ecaf2fb3a77cf6e49189ba3942
kelvinvaghasiya <kelvin.vaghasiya@mrisoftware.com>
2024-10-28 18:11:52 +0530

    Enabled login functionalities and contexts

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
 // import Cookies from 'js-cookie;'
 import '../styles/Login.css'; // Import the CSS file
-
-import * as helpers from "../Utils/validation";
-import * as cookie from "../Utils/cookie";
-// import * as helpers from "../Utils/validation";
 import { loginValidation } from '../Utils/validation';
 import axios from "axios"
 const server = import.meta.env.VITE_SERVER;
+import { Navigate } from "react-router-dom";
+import { useUserInfo } from '../Contexts/User';
+import { setCookie } from '../Utils/cookie';
+
 
 
 
 const Login = () => {
     const [formData, setFormData] = useState({ email: '', password: '' });
     const [errors, setErrors] = useState({ email: '', password: '' });
+    const {loggedIn , setLoggedIn} = useUserInfo();
 
     const handleChange = (event) => {
         setFormData((prevValue) => {
@@ -40,8 +41,9 @@ const Login = () => {
                     Password : formData.password,
                     Email : formData.email
                 });
-                console.log(response);
-                
+                console.log(response.data);
+                setLoggedIn(true);
+                setCookie("Auth_Token",response.data.token,1);
             } catch (error) {
                 console.log(error);
                 
@@ -84,6 +86,9 @@ const Login = () => {
                     <button type='submit' onClick={handleSubmit} className="btn btn-primary w-100">Login</button>
                 </form>
             </div>
+            {
+                (loggedIn) && <Navigate to="/" replace={true} />
+            }
         </div>
     );
 };
