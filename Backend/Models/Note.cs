cd21db037d5f4b70b47da52ea9d6a54b90cb3852
chiraggavande-mri <chirag.gavande@mrisoftware.com>
2024-10-30 16:10:03 +0530

    last commit max date

--- Backend/Models/Note.cs
+++ Backend/Models/Note.cs
@@ -32,7 +32,7 @@ namespace Backend.Models
         [Required]
         public NoteStatus Status { get; set; }
 
-        public DateTime? ExpectedCompletion { get; set; } = null;
+        public DateTime? ExpectedCompletion { get; set; } = DateTime.MaxValue;
 
         public int CreatedBy { get; set; } // Foreign key for the user who created the note
 
