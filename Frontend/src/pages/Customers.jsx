63920c9fbd3245c374a16642dbcc816ae0623a3a
Nilay Kantharia <nilaykumar.kantharia@mrisoftware.com>
2024-10-30 12:23:48 +0530

    minor changes

--- Frontend/src/pages/Customers.jsx
+++ Frontend/src/pages/Customers.jsx
@@ -8,32 +8,33 @@ import ConvertDate from '../Utils/ConvertDate';
 const server = import.meta.env.VITE_SERVER;
 
 function Customers() {
-    const {contextUser} = useUserInfo();
-    const [customerList , setCustomerList] = useState(null);
+    const { contextUser } = useUserInfo();
+    const [customerList, setCustomerList] = useState(null);
     console.log(customerList);
-    
 
-    useEffect(()=>{
-        const getCustomerData = async ()=>{
+
+    useEffect(() => {
+        const getCustomerData = async () => {
             const Auth_Token = getCookie("Auth_Token")
-            const response = await axios.get(`${server}/api/Customers`,{
-                headers : {
-                    Authorization : `Bearer ${Auth_Token}`
+            const response = await axios.get(`${server}/api/Customers`, {
+                headers: {
+                    Authorization: `Bearer ${Auth_Token}`
                 }
             })
 
             console.log(response);
             setCustomerList(response.data)
-            
+
         }
         getCustomerData();
-    },[])
+    }, [])
     return (
         <>
-            <Navbar ifAdmin={(contextUser?.role===0)?true:false} />
+            <Navbar ifAdmin={(contextUser?.role === 0) ? true : false} />
             <div className="mx-sm-5">
-                <div className="options">
-                    <ul className="nav justify-content-md-end mt-3">
+                <div className="options d-flex">
+                <h3 className='mt-3'>Customer List</h3>
+                    <ul className="nav ms-auto justify-content-md-end mt-3">
                         <li className="nav-item">
                             <button className="btn btn-primary mx-3" aria-current="page" href="#">Add</button>
                         </li>
@@ -47,10 +48,8 @@ function Customers() {
                     </ul>
                 </div>
 
-                <h3>Customer List</h3>
-
                 {
-                    customerList?.map ((item,index)=>{
+                    customerList?.map((item, index) => {
                         const Date = ConvertDate(item.createdAt);
                         return <CustomerEntry key={index} cid={item.cid} name={`${item.firstName} ${item.lastName}`} CreatedBy={item.createdBy} CreatedAt={Date} />
                     })
